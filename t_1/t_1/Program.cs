593ebe7c02932d5734b9ff2d2956e02bd7681521
SarvadiDadina <darsinema@gmail.com>
2020-02-24 10:04:12 +0200

    lab4

---
+++ t_1/t_1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace t_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("a = ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("b = ");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Angle = ");
+            double angle = Convert.ToInt32(Console.ReadLine());
+            double result = 0.5 * a * b * Math.Sin((angle * Math.PI) / 180);
+            Console.WriteLine("Площа трикутника = {0}", result);
+        }
+    }
+}
---
+++ t_2/t_2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace t_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("a = ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("b = ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.Write("c = ");
+            int c = Convert.ToInt32(Console.ReadLine());
+            if (a < 9 && a > 3 || a > b && a < c)
+                Console.WriteLine("Належить");
+            else
+                Console.WriteLine("Неналежить");
+
+        }
+    }
+}
